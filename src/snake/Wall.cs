8b5a8a4b01c09f1338b31cf4c81f9183f106faa4
Toshinaki <shaduking@hotmail.com>
2021-12-30 00:17:44 +0800

    apply gitignore

--- src/snake/Wall.cs
+++
@@ -1,6 +0,0 @@
-namespace snake {
-    internal class Wall : GameObject {
-        public Wall (int[, ] tilePos, string tileSymbol = TileSymbol.FULL_BLOCK2) : base (tilePos, tileSymbol) { }
-        public Wall (List<int[]> tilePos, string tileSymbol = TileSymbol.FULL_BLOCK2) : base (tilePos, tileSymbol) { }
-    }
-}
